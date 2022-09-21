422be00e9c487e0a3a96f1344761ee6f9c7626a8
bhadra-123 <bhadra.mangunuru@gmail.com>
2022-09-21 03:09:22 +0000

    added and deleted files

---
+++ prod/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  resource_prefix = "${var.Environment}-${var.location_short}"
+  Owner       = var.Owner
+  Reason      = var.Reason
+  Environment = var.Environment
+  common_tags = {
+    Owner       = local.Owner
+    Reason      = local.Reason
+    Environment = local.Environment
+  }
+}
+
